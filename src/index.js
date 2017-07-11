b70b0cedf3f2f5aef35b212c71942706d7b6d0e1
Hibikine Kage <goodsmile314159@gmail.com>
2017-07-11 23:05:01 +0900

    Delete testcode

--- src/index.js
+++ src/index.js
@@ -4,21 +4,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { selectSubreddit, fetchPostsIfNeeded } from './actions'
 import rootReducer from './reducers'
 
-const loggerMiddleware = createLogger();
-
 /**
  * Store for fetch subreddit's posts.
  * @type {Store}
  */
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    thunkMiddleware,
-    loggerMiddleware,
-  )
-);
-
-store.dispatch(selectSubreddit('reactjs'));
-store
-  .dispatch(fetchPostsIfNeeded('reactjs'))
-  .then(() => console.log(store.getState()));
